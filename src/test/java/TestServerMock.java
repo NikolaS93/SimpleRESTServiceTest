f477ee98b3eb9849ee82ce2895be0173b1d33a24
NikolaS93 <82213318+NikolaS93@users.noreply.github.com>
2022-07-28 20:33:59 +0200

    update main

--- src/test/java/TestServerMock.java
+++ src/test/java/TestServerMock.java
@@ -9,16 +9,20 @@ import static org.hamcrest.Matchers.*;
 
 public class TestServerMock {
 
+    public final String baseURI = "http://metadata-server-mock.herokuapp.com/";
+    public final String signature = "2bf86524f8cb8dff2d06ea291cccb6bd69cd31499d64eac5d661245d6c68ebe047515f0e119ddf08848d20e7b898dda59b1369bcb1dc59210049c76d00db9f04";
+    public final String publicKey = "9b3c4095df24e08599115c750988b0a105043cd15b6521a123f21d7b92369a73";
+    public final String metadataValue = "2048c7e09308f9138cef8f1a81733b72e601d016eea5eef759ff2933416d617a696e67436f696e";
+
+
     @Test
     public void postQuery() {
 
-        String baseURI = "http://metadata-server-mock.herokuapp.com/";
         String endpoint = "metadata/query";
 
         Signature[] signatures = new Signature[1];
 
-        signatures[0] = new Signature("789ef8ae89617f34c07f7f6a12e4d65146f958c0bc15a97b4ff169f16861707079636f696e",
-                "789ef8ae89617f34c07f7f6a12e4d65146f958c0bc15a97b4ff169f1");
+        signatures[0] = new Signature(this.signature, this.publicKey);
 
         RestAssured.
                 given().
@@ -28,15 +32,13 @@ public class TestServerMock {
                 then().
                     assertThat().statusCode(500).
                         log().body();
-
-
     }
 
     @Test
     public void getMetadata() {
 
-        String baseURI = "http://metadata-server-mock.herokuapp.com/";
-        String endpoint = "metadata/2048c7e09308f9138cef8f1a81733b72e601d016eea5eef759ff2933416d617a696e67436f696e";
+//        String endpoint = "metadata/2048c7e09308f9138cef8f1a81733b72e601d016eea5eef759ff2933416d617a696e67436f696e";
+        String endpoint = String.format("metadata/%s", metadataValue);
 
         RestAssured.
                 given().
@@ -45,40 +47,19 @@ public class TestServerMock {
                 then().
                 assertThat().
                 statusCode(200).
-                body("subject", equalTo("2048c7e09308f9138cef8f1a81733b72e601d016eea5eef759ff2933416d617a696e67436f696e"));
+                body("subject", equalTo(metadataValue));
 
     }
 
     @Test
     public void getMetadataProperty() {
 
-        String baseURI = "http://metadata-server-mock.herokuapp.com/";
-        String endpoint = "metadata/2048c7e09308f9138cef8f1a81733b72e601d016eea5eef759ff2933416d617a696e67436f696e/properties/name";
 
-        RestAssured.
-                given().
-                when().
-                    get(baseURI + endpoint).
-                then().
-                    log().body().
-                    assertThat().
-                        statusCode(200).
-                        header("Content-Type", equalTo("application/json")).
-                        body("sequenceNumber.", notNullValue()).
-                        body("value", equalTo("Amazing Coin")).
-                        body("signatures.size()", greaterThan(0));
-    }
-
-    @Test
-    public void getMetadataPropertyDeserialized() {
-
-        String baseURI = "http://metadata-server-mock.herokuapp.com/";
-        String endpoint = "metadata/2048c7e09308f9138cef8f1a81733b72e601d016eea5eef759ff2933416d617a696e67436f696e/properties/name";
+        String endpoint = String.format("metadata/%s/properties/name", metadataValue);
 
         Signature[] signatures = new Signature[1];
 
-        signatures[0] = new Signature("2bf86524f8cb8dff2d06ea291cccb6bd69cd31499d64eac5d661245d6c68ebe047515f0e119ddf08848d20e7b898dda59b1369bcb1dc59210049c76d00db9f04",
-                "9b3c4095df24e08599115c750988b0a105043cd15b6521a123f21d7b92369a73");
+        signatures[0] = new Signature(signature, publicKey);
 
         Coin expectedCoin = new Coin(0, "Amazing Coin", signatures);
 
